69efd95d2c1f132551ffd0f0f692f361588e8f7f
gregory.feijon <gregory.feijon@outlook.com>
2021-06-26 23:14:33 -0300

    [UPDATE] Add CORS.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
+import cors from "cors";
 
 import { router } from "./routes";
 
@@ -9,6 +10,8 @@ import { errorHandler } from "./shared/utils/ErrorHandler";
 import { BaseError } from "./exceptions/BaseError";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
